f335fe28bbcd3dba3b80d79fe56c1944a175abc6
Ana Luiza Bohrer <ana.bohrer@hammerconsult.com.br>
2020-11-02 20:39:59 -0300

    Nova tentativa

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+const path = require("path");
 var nomeApp = process.env.npm_package_name;
-app.use(express.static('./dist/'+ nomeApp));
+app.use(express.static('./dist/ContasAPagar'));
+const favicon = require("serve-favicon");
 
+app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')))
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/' + nomeApp + '/'});});
+  res.sendFile('index.html', {root: 'dist/ContasAPagar/'});});
 
 app.listen(process.env.PORT || 8080);
 
