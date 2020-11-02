04389129f62bd2fb7cc6cd7cd9b7bbe2424a5be3
Ana Luiza Bohrer <ana.bohrer@hammerconsult.com.br>
2020-11-02 15:18:13 -0300

    Mais uma tentativa

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${C:/Users/AnaLuizaBohrer/source/repos/ContasAPagar}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${C:/Users/AnaLuizaBohrer/source/repos/ContasAPagar}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
